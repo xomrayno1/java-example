cf74973678f70a8477648bd0bac60ee5559860fc
xomrayo1 <xomrayno5@gmail.com>
2021-07-18 16:40:31 +0700

    initial project

---
+++ src/main/java/com/tampro/Elasticsearch02Application.java
@@ -0,0 +1,13 @@
+package com.tampro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elasticsearch02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Elasticsearch02Application.class, args);
+	}
+
+}
